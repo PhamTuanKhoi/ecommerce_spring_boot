e6ed99785c2aea8804cf2a2709168e194a363fed
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2023-08-05 20:14:28 +0700

    user service detail

---
+++ src/test/java/com/v1/ecommerce/service/CustomUserServiceImplementation.java
@@ -0,0 +1,30 @@
+package com.v1.ecommerce.service;
+
+import com.v1.ecommerce.model.User;
+import com.v1.ecommerce.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomUserServiceImplementation implements UserDetailsService {
+    public UserRepository userRepository;
+
+    public CustomUserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("user not found by email - " + username);
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
