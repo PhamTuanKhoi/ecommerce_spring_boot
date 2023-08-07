7a4120e51dbd0244ea0a690e53a183df7921a920
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2023-08-07 10:37:30 +0700

    import user service impl

--- src/main/java/com/v1/ecommerce/service/UserService.java
+++ src/main/java/com/v1/ecommerce/service/UserService.java
@@ -4,6 +4,6 @@ import com.v1.ecommerce.exception.UserException;
 import com.v1.ecommerce.model.User;
 
 public interface UserService {
-    public User findUserById(Long userId) throws UserException;
+    public User findUserById(Long id) throws UserException;
     public User findUserProviderByJwt(String jwt) throws UserException;
 }
---
+++ src/main/java/com/v1/ecommerce/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.v1.ecommerce.service.impl;
+
+import com.v1.ecommerce.config.JwtProvider;
+import com.v1.ecommerce.exception.UserException;
+import com.v1.ecommerce.model.User;
+import com.v1.ecommerce.repository.UserRepository;
+import com.v1.ecommerce.service.UserService;
+
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    private JwtProvider jwtProvider;
+
+    public UserServiceImpl(UserRepository userRepository, JwtProvider jwtProvider) {
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public User findUserById(Long id) throws UserException {
+        Optional<User> user = this.userRepository.findById(id);
+
+        if(user == null){
+            throw new UserException("User not found by id# " + id);
+        }
+        return user.get();
+    }
+
+    @Override
+    public User findUserProviderByJwt(String jwt) throws UserException {
+        String email = this.jwtProvider.getEmailFromToken(jwt);
+
+        User user = this.userRepository.findByEmail(email);
+
+        if(user == null){
+            throw new UserException("User not found by email# " + email);
+        }
+        return user;
+    }
+}
